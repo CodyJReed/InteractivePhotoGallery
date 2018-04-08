bbb536de8e9ffe18dca146a777bd53a3e7214892
Cody Reed <cody2j2reed@gmail.com>
2018-04-07 20:23:41 -0400

    finalized project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('.lightbox').rebox({
+  selector: 'a'
+});
+
+(function() {
+  var $imgs = $('.lightbox img');       // Get the images
+  var $search = $('#search');           // Get the input elements
+  var cache = [];                       // Create an array called cache
+
+  $imgs.each(function() {                     // For each image
+    cache.push({                              // Add an object to the cache array
+      element: this,                          // This image
+      text: this.title.trim().toLowerCase()   // Its title text (lowercase trimmed)
+    });
+  });
+
+  function filter() {                             // Declare filter() function
+    var query = this.value.trim().toLowerCase();  //Get the query
+
+    cache.forEach(function(img) {           // For each entry in cache pass image
+        var index = 0;                      // Set index to 0
+        if (query) {                        // If there is some query text
+          index = img.text.indexOf(query);  // Find if query text is in there
+        }
+
+        img.element.style.display = index === -1 ? 'none' : '';  // Show / hide
+      });
+    }
+
+    if ('oninput' in $search[0]) {    // If browser supports input event
+      $search.on('input', filter);    // Use input event to call filter()
+    } else {                          // Otherwise
+      $search.on('keyup', filter);     // Use keyup event to call filter()
+
+    }
+}());
